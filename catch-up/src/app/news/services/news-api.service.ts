e734e0fe69649a9f4262eb30547bba820bec8f2a
Andres-0304 <83299190+Andres-0304@users.noreply.github.com>
2025-04-30 15:43:52 -0500

    Add files via upload

---
+++ catch-up/src/app/news/services/news-api.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import {environment} from '../../../environments/environment';
+import {HttpClient} from '@angular/common/http';
+import {LogoApiService} from '../../shared/services/logo-api.service';
+import {SourcesResponse} from './sources.response';
+import {Source} from '../model/source.entity';
+import {map, Observable} from 'rxjs';
+import {SourcesAssembler} from './sources.assembler';
+import {Article} from '../model/article.entity';
+import {TopHeadlinesResponse} from './top-headlines.response';
+import {ArticleAssembler} from './article.assembler';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsApiService {
+  private baseUrl  = environment.newsProviderApiBaseUrl;
+  private newsEndpoint = environment.newsProviderNewsEndpointPath;
+  private sourcesEndpoint = environment.newsProviderSourcesEndpointPath;
+  private apiKey = environment.newsProviderApiKey;
+  constructor(private http: HttpClient, private logoApiService: LogoApiService) { }
+
+  getSources(): Observable<Source[]> {
+    return this.http.get<SourcesResponse>(`${this.baseUrl}${this.sourcesEndpoint}`,{
+      params : { apiKey: this.apiKey  }
+    }).pipe(
+      map(response => SourcesAssembler.withLogoApiService(this.logoApiService).toEntitiesFromResponse(response))
+    );
+  }
+
+  getArticleBySourceId(sourceId: string): Observable<Article[]> {
+    return this.http.get<TopHeadlinesResponse>(`${this.baseUrl}${this.newsEndpoint}`,{
+      params : { apiKey: this.apiKey , sources: sourceId }
+    }).pipe(
+      map(response => ArticleAssembler.withLogoApiService(this.logoApiService).toEntitiesFromResponse(response))
+    );
+  }
+}
---
+++ catch-up/src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  newsProviderApiBaseUrl: 'https://newsapi.org/v2',
+  newsProviderNewsEndpointPath: '/top-headlines',
+  newsProviderSourcesEndpointPath: '/top-headlines/sources',
+  newsProviderApiKey: '7dd4442ad84642768511a09c74f5ff5b',
+  logoProviderApiBaseUrl: 'https://logo.clearbit.com/'
+};
