f668d6dd885cea46a7b1786c9a13ef0264e56344
Andres-0304 <83299190+Andres-0304@users.noreply.github.com>
2025-04-30 19:15:45 -0500

    Delete catch-up directory

--- catch-up/src/app/news/model/source.entity.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Source } from './source.entity';
-
-describe('Source', () => {
-  it('should create an instance', () => {
-    expect(new Source()).toBeTruthy();
-  });
-});
--- catch-up/src/app/news/services/sources.assembler.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Sources } from './sources.assembler';
-
-describe('Sources', () => {
-  it('should create an instance', () => {
-    expect(new Sources()).toBeTruthy();
-  });
-});
